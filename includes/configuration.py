844d7d68dd53e10f0aa834601aa9bf72595862de
Ypaspistis Giannis <119696474+giannisyp@users.noreply.github.com>
2023-11-04 12:28:44 +0200

    Delete includes directory

--- includes/configuration.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-raw_folder_path = "/mnt/formula1giannisdl/raw"
-processed_folder_path = "/mnt/formula1giannisdl/processed"
-presentation_folder_path = "/mnt/formula1giannisdl/presentation"
-
-# COMMAND ----------
-
